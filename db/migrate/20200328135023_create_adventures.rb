815e4767e8bf30ec7ac3b2870fcc49273197410d
Paulo DAlberti <phdalberti@gmail.com>
2020-04-08 20:12:25 +0900

    prepare grounds for chapter rendering

--- db/migrate/20200328135023_create_adventures.rb
+++ db/migrate/20200328135023_create_adventures.rb
@@ -1,7 +1,7 @@
 class CreateAdventures < ActiveRecord::Migration[6.0]
   def change
     create_table :adventures do |t|
-      t.float :current_chapter
+      t.float :current_chapter, default: 1.1
       t.string :load_code
       t.string :email
       t.boolean :test
