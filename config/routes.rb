7cea4a9b7ed4fb5f4e4727098628803c3448bc2e
Paulo DAlberti <phdalberti@gmail.com>
2020-04-13 17:46:40 +0900

    renamed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
-  resources :adventures, only: %i[index show new create edit update] do
+  resources :adventures,
+            only: %i[index show new create edit update],
+            path_names: { edit: 'ongoing_adventure' } do
     resources :player_characters, only: %i[new create]
     # resources :non_player_characters
   end
