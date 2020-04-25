7714ee18a924ee425a7def263bc64bc2dc260998
Paulo DAlberti <phdalberti@gmail.com>
2020-04-25 13:45:25 +0900

    small change to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 adventure = Adventure.new(test: true, player_amount: 3)
 adventure.save!
 adventure.player_amount.times do
   pc = PlayerCharacter.new(player_name: Faker::Name.name,
                            name: Faker::Games::Heroes.name,
-                           race: 'Human',
+                           race: %w[Human Dwarf Elf Halfling].sample,
                            path: Faker::Games::Heroes.klass)
   pc.adventure_id = adventure.id
   pc.save!
